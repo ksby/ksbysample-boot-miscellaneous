50e2c46f29b72e7fab5e57f14e92cc3abf7b94b4
ksby <kusa@db3.so-net.ne.jp>
2017-08-17 06:52:14 +0900

    #1 Flyway をインストールし、in-memory モードの H2 Database に IDEA の Database tools から接続するための H2 の TCP サーバを起動する JavaConfig クラスと設定を追加しました

--- boot-npm-geb-sample/src/main/java/ksbysample/webapp/bootnpmgeb/config/WebSecurityConfig.java
+++ boot-npm-geb-sample/src/main/java/ksbysample/webapp/bootnpmgeb/config/WebSecurityConfig.java
@@ -1,10 +1,13 @@
 package ksbysample.webapp.bootnpmgeb.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.util.regex.Pattern;
 
 /**
  * ???
@@ -12,6 +15,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final Pattern H2_CONSOLE_URI_PATTERN = Pattern.compile("^/h2-console");
+
+    @Value("${spring.h2.console.enabled:false}")
+    private boolean springH2ConsoleEnabled;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -24,6 +32,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // 全ての URL を認証の対象外にする
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated();
+
+        // spring.h2.console.enabled=true に設定されている場合には H2 Console を表示するために必要な設定を行う
+        if (springH2ConsoleEnabled) {
+            http.csrf()
+                    .requireCsrfProtectionMatcher(request -> {
+                        // H2 Console は CSRF対策の対象外にする
+                        if (H2_CONSOLE_URI_PATTERN.matcher(request.getRequestURI()).lookingAt()) {
+                            return false;
+                        }
+                        return true;
+                    });
+            http.headers().frameOptions().sameOrigin();
+        }
     }
 
 }
