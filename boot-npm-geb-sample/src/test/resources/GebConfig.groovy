557779c28e4c5f538d1b74967e5088b63d5b21d7
ksby <kusa@db3.so-net.ne.jp>
2017-10-28 08:47:10 +0900

    #1 geckodriver.exe の場所は `System.setProperty("webdriver.gecko.driver", "...")` で指定するようにしました

--- boot-npm-geb-sample/src/test/resources/GebConfig.groovy
+++ boot-npm-geb-sample/src/test/resources/GebConfig.groovy
@@ -1,4 +1,9 @@
-driver = "firefox"
+import org.openqa.selenium.firefox.FirefoxDriver
+
+driver = {
+    System.setProperty("webdriver.gecko.driver", "C:/geckodriver/0.19.0/geckodriver.exe")
+    new FirefoxDriver()
+}
 baseUrl = "http://localhost:8080"
 waiting {
     timeout = 15
