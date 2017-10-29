3510e4472d8a2fd2e5412cde67e49da6bddf3425
ksby <kusa@db3.so-net.ne.jp>
2017-10-29 09:35:22 +0900

    #1 テスト対象のブラウザに Headless Chrome と HtmlUnit を追加し、Chrome, Firefox, HtmlUnit で連続テストする gradle タスクを追加しました

--- boot-npm-geb-sample/src/test/resources/GebConfig.groovy
+++ boot-npm-geb-sample/src/test/resources/GebConfig.groovy
@@ -1,8 +1,12 @@
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxOptions
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
 
+System.setProperty("webdriver.gecko.driver", "C:/geckodriver/0.19.0/geckodriver.bat")
+System.setProperty("webdriver.chrome.driver", "C:/chromedriver/2.33/chromedriver.exe")
 driver = {
-    System.setProperty("webdriver.gecko.driver", "C:/geckodriver/0.19.0/geckodriver.bat")
     FirefoxOptions firefoxOptions = new FirefoxOptions()
     firefoxOptions.setHeadless(true)
     new FirefoxDriver(firefoxOptions)
@@ -10,4 +14,28 @@ driver = {
 baseUrl = "http://localhost:8080"
 waiting {
     timeout = 15
+}
+
+environments {
+
+    chrome {
+        driver = {
+            ChromeOptions chromeOptions = new ChromeOptions()
+            chromeOptions.setHeadless(true)
+            new ChromeDriver(chromeOptions)
+        }
+    }
+
+    firefox {
+        driver = {
+            FirefoxOptions firefoxOptions = new FirefoxOptions()
+            firefoxOptions.setHeadless(true)
+            new FirefoxDriver(firefoxOptions)
+        }
+    }
+
+    htmlunit {
+        new HtmlUnitDriver(true)
+    }
+
 }
