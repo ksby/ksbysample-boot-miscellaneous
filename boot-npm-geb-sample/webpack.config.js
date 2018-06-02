a8a83e283593acaaee86b52548ca589353e0e1d8
ksby <kusa@db3.so-net.ne.jp>
2018-06-02 12:45:34 +0900

    #1 webpack を 3.8.1 → 4.9.1 へバージョンアップしました

--- boot-npm-geb-sample/webpack.config.js
+++ boot-npm-geb-sample/webpack.config.js
@@ -1,51 +1,65 @@
 const webpack = require("webpack");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const isProduct = process.env.NODE_ENV === "product";
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-    entry: {
-        "js/inquiry/input01": ["./src/main/assets/js/inquiry/input01.js"],
-        "js/inquiry/input02": ["./src/main/assets/js/inquiry/input02.js"],
-        "js/inquiry/input03": ["./src/main/assets/js/inquiry/input03.js"],
-        "js/inquiry/confirm": ["./src/main/assets/js/inquiry/confirm.js"]
-    },
-    output: {
-        path: __dirname + "/src/main/resources/static",
-        publicPath: "/",
-        filename: "[name].js"
-    },
-    resolve: {
-        modules: [
-            "node_modules",
-            "src/main/assets/js"
-        ],
-        alias: {
-            jquery: "jquery"
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: [
-                    /node_modules/,
-                    /jquery.autoKana.js$/
-                ],
-                loader: "eslint-loader"
+// --mode オプションで指定された文字列を参照したい場合には argv.mode を参照する
+module.exports = (env, argv) => {
+    return {
+        entry: {
+            "js/inquiry/input01": ["./src/main/assets/js/inquiry/input01.js"],
+            "js/inquiry/input02": ["./src/main/assets/js/inquiry/input02.js"],
+            "js/inquiry/input03": ["./src/main/assets/js/inquiry/input03.js"],
+            "js/inquiry/confirm": ["./src/main/assets/js/inquiry/confirm.js"]
+        },
+        output: {
+            path: __dirname + "/src/main/resources/static",
+            publicPath: "/",
+            filename: "[name].js"
+        },
+        resolve: {
+            modules: [
+                "node_modules",
+                "src/main/assets/js"
+            ],
+            alias: {
+                jquery: "jquery"
             }
-        ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        })
-    ].concat(
-        isProduct
-            ? [
-                new UglifyJsPlugin()
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: [
+                        /node_modules/,
+                        /jquery.autoKana.js$/
+                    ],
+                    loader: "eslint-loader"
+                }
             ]
-            : []
-    ),
-    devtool: "inline-source-map"
+        },
+        optimization: {
+            minimizer: [
+                new UglifyJsPlugin({
+                    uglifyOptions: {
+                        compress: true,
+                        ecma: 5,
+                        output: {
+                            comments: false
+                        },
+                        compress: {
+                            dead_code: true,
+                            drop_console: true
+                        }
+                    },
+                    sourceMap: false
+                })
+            ]
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery"
+            })
+        ],
+        devtool: "inline-source-map"
+    };
 };
