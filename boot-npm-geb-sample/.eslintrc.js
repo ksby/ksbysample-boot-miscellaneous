b77c00b814655793fedea5c838d4793a91e9c8a0
ksby <kusa@db3.so-net.ne.jp>
2018-07-16 14:27:11 +0900

    #1 Prettier だけでは Javascirpt のフォーマット以外の問題点を検知できないことに気づいたので、再び eslint-config-airbnb-base をインストールしました

--- boot-npm-geb-sample/.eslintrc.js
+++ boot-npm-geb-sample/.eslintrc.js
@@ -1,3 +1,27 @@
 module.exports = {
-  extends: "plugin:prettier/recommended"
+  extends: ["airbnb-base/legacy", "plugin:prettier/recommended"],
+  env: {
+    jquery: true
+  },
+  rules: {
+    // requires to declare all vars on top of their containing scope
+    "vars-on-top": "off",
+
+    // require function expressions to have a name
+    // http://eslint.org/docs/rules/func-names
+    "func-names": "off",
+
+    // disallow mixed 'LF' and 'CRLF' as linebreaks
+    // http://eslint.org/docs/rules/linebreak-style
+    "linebreak-style": ["error", "windows"],
+
+    // specify whether double or single quotes should be used
+    quotes: ["error", "double", { avoidEscape: true }],
+
+    // disallow use of variables before they are defined
+    "no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true }
+    ]
+  }
 };
