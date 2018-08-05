e160516446ff6ba1fd7ab4615d03af0fe4806170
ksby <kusa@db3.so-net.ne.jp>
2018-08-05 13:15:04 +0900

    #2 コネクションプーリング用ライブラリを Tomcat connection pool → HikariCP に切り替えました

--- boot-npm-geb-sample/src/main/java/ksbysample/webapp/bootnpmgeb/config/ApplicationConfig.java
+++ boot-npm-geb-sample/src/main/java/ksbysample/webapp/bootnpmgeb/config/ApplicationConfig.java
@@ -1,10 +1,13 @@
 package ksbysample.webapp.bootnpmgeb.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.jmx.support.RegistrationPolicy;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
@@ -14,6 +17,7 @@ import javax.sql.DataSource;
  * ???
  */
 @Configuration
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 public class ApplicationConfig {
 
     private final MessageSource messageSource;
@@ -40,13 +44,13 @@ public class ApplicationConfig {
     }
 
     /**
-     * @return Tomcat JDBC Connection Pool の DataSource オブジェクト
+     * @return HikariCP の DataSource オブジェクト
      */
     @Bean
-    @ConfigurationProperties("spring.datasource.tomcat")
+    @ConfigurationProperties("spring.datasource.hikari")
     public DataSource dataSource() {
         return DataSourceBuilder.create()
-                .type(org.apache.tomcat.jdbc.pool.DataSource.class)
+                .type(HikariDataSource.class)
                 .build();
     }
 
