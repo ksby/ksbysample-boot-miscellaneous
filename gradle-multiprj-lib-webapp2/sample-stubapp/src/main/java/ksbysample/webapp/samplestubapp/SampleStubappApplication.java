27a5f4c0fa64c524ca3d28a55977d483438c294c
ksby <kusa@db3.so-net.ne.jp>
2019-04-21 10:32:06 +0000

    #12 Spring Boot ベースの Web アプリケーションとして sample-stubapp プロジェクトを作成しました

---
+++ gradle-multiprj-lib-webapp2/sample-stubapp/src/main/java/ksbysample/webapp/samplestubapp/SampleStubappApplication.java
@@ -0,0 +1,13 @@
+package ksbysample.webapp.samplestubapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleStubappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleStubappApplication.class, args);
+    }
+
+}
