0b9715a5ec60895f969b1b7726d8f38e33e3af9c
ksby <kusa@db3.so-net.ne.jp>
2019-04-23 22:38:28 +0000

    #13 sample-webapp プロジェクトを新規作成しました

---
+++ gradle-multiprj-includeonly/sample-webapp/src/test/java/ksbysample/webapp/samplewebapp/SampleWebappApplicationTests.java
@@ -0,0 +1,16 @@
+package ksbysample.webapp.samplewebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleWebappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
