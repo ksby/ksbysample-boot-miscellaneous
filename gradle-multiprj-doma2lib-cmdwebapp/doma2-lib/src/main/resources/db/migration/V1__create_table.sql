9d1068edc829fa2a443871df9c1ebbe745a229f5
ksby <kusa@db3.so-net.ne.jp>
2019-04-30 02:17:25 +0000

    #14 employee テーブルに age, sex, update_time のカラムを追加しました

--- gradle-multiprj-doma2lib-cmdwebapp/doma2-lib/src/main/resources/db/migration/V1__create_table.sql
+++ gradle-multiprj-doma2lib-cmdwebapp/doma2-lib/src/main/resources/db/migration/V1__create_table.sql
@@ -1,7 +1,10 @@
 create table employee (
     id int(11) not null auto_increment,
     name varchar(128) not null,
+    age int(3) not null,
+    sex enum('男', '女'),
+    update_time datetime default current_timestamp,
     primary key (id)
 );
-insert into employee (name) values ('tanaka taro');
-insert into employee (name) values ('suzuki hanako');
+insert into employee (name, age, sex) values ('田中　太郎', 20, '男');
+insert into employee (name, age, sex) values ('鈴木　花子', 18, '女');
