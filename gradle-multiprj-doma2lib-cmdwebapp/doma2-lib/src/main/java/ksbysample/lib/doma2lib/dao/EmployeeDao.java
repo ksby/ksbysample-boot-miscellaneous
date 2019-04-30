3e450c4a1490405fc22f3ff2d9f4e2239c1bf5cf
ksby <kusa@db3.so-net.ne.jp>
2019-04-30 02:46:10 +0000

    #13 domaGen タスクを実行して employee テーブルの Dao インターフェース、Entity クラスを生成しました

---
+++ gradle-multiprj-doma2lib-cmdwebapp/doma2-lib/src/main/java/ksbysample/lib/doma2lib/dao/EmployeeDao.java
@@ -0,0 +1,44 @@
+package ksbysample.lib.doma2lib.dao;
+
+import ksbysample.lib.doma2lib.entity.Employee;
+import org.seasar.doma.boot.ConfigAutowireable;
+import org.seasar.doma.Dao;
+import org.seasar.doma.Delete;
+import org.seasar.doma.Insert;
+import org.seasar.doma.Select;
+import org.seasar.doma.Update;
+
+/**
+ */
+@Dao
+@ConfigAutowireable
+public interface EmployeeDao {
+
+    /**
+     * @param id
+     * @return the Employee entity
+     */
+    @Select
+    Employee selectById(Integer id);
+
+    /**
+     * @param entity
+     * @return affected rows
+     */
+    @Insert
+    int insert(Employee entity);
+
+    /**
+     * @param entity
+     * @return affected rows
+     */
+    @Update
+    int update(Employee entity);
+
+    /**
+     * @param entity
+     * @return affected rows
+     */
+    @Delete
+    int delete(Employee entity);
+}
