e8dedf2ace047ecb8cfb24985205297de24cd4b8
ksby <kusa@db3.so-net.ne.jp>
2019-04-30 05:07:43 +0000

    #14 sampledb の collate の設定を utf8mb4_0900_as_cs → utf8mb4_ja_0900_as_cs_ks に変更しました

--- gradle-multiprj-doma2lib-cmdwebapp/doma2-lib/src/main/resources/db/init/create_database.sql
+++ gradle-multiprj-doma2lib-cmdwebapp/doma2-lib/src/main/resources/db/init/create_database.sql
@@ -1,4 +1,4 @@
-create database if not exists sampledb character set utf8mb4 collate utf8mb4_0900_as_cs;
+create database if not exists sampledb character set utf8mb4 collate utf8mb4_ja_0900_as_cs_ks;
 
 create user 'sampledb_user'@'%' identified by 'xxxxxxxx';
 grant all privileges ON sampledb.* to 'sampledb_user'@'%' with grant option;
