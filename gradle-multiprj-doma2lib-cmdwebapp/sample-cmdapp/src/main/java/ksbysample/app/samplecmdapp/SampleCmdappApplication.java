1310bfae1fedba95235f67eae6aa8559fe25ce30
ksby <kusa@db3.so-net.ne.jp>
2019-04-30 09:56:22 +0000

    #14 sample-cmdapp サブプロジェクトを新規作成しました

---
+++ gradle-multiprj-doma2lib-cmdwebapp/sample-cmdapp/src/main/java/ksbysample/app/samplecmdapp/SampleCmdappApplication.java
@@ -0,0 +1,13 @@
+package ksbysample.app.samplecmdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCmdappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleCmdappApplication.class, args);
+    }
+
+}
