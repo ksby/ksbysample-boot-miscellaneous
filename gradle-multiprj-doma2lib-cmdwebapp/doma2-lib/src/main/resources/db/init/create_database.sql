588beb7d8452f56c170f41ec39a6e5437d9738c5
ksby <kusa@db3.so-net.ne.jp>
2019-05-01 02:50:22 +0000

    #14 アプリケーション実行時に flyway が performance_schema.user_variables_by_thread テーブルにアクセスしようとして java.sql.SQLSyntaxErrorException を発生させていたので、select 権限を付与しました

--- gradle-multiprj-doma2lib-cmdwebapp/doma2-lib/src/main/resources/db/init/create_database.sql
+++ gradle-multiprj-doma2lib-cmdwebapp/doma2-lib/src/main/resources/db/init/create_database.sql
@@ -2,4 +2,5 @@ create database if not exists sampledb character set utf8mb4 collate utf8mb4_ja_
 
 create user 'sampledb_user'@'%' identified by 'xxxxxxxx';
 grant all privileges ON sampledb.* to 'sampledb_user'@'%' with grant option;
+grant select ON performance_schema.user_variables_by_thread to 'sampledb_user'@'%';
 flush privileges;
