2600e48b7625dee15695db6d83c791e894ca53fd
ksby <kusa@db3.so-net.ne.jp>
2019-05-06 11:24:02 +0000

    #15 frontend-app に WebAPI を呼び出して取得したデータを画面に表示する処理を実装しました

---
+++ gradle-multiprj-vuejs-springboot/frontend-app/src/components/CallSampleWebapi.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-layout text-xs-center wrap>
+      <v-flex xs12>
+        <div>code: {{ code }}</div>
+        <div>value: {{ value }}</div>
+        <v-btn color="info" v-on:click="callSampleWebapi">クリック！</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      code: "(空)",
+      value: "(空)"
+    };
+  },
+  methods: {
+    callSampleWebapi: async function() {
+      try {
+        const response = await axios.post("/webapi/sample");
+        this.$set(this, "code", response.data.code);
+        this.$set(this, "value", response.data.value);
+      } catch (err) {
+        alert(err);
+      }
+    }
+  }
+};
+</script>
+
+<style></style>
